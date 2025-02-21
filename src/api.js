7ab4944e6bf99399d0519d1a9572a2f533b10d3e
somendrajain <somendrajain1799@gmail.com>
2025-02-21 20:18:15 +0530

    updated APi URL

--- src/api.js
+++ src/api.js
@@ -1,11 +1,14 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8000/api/tenders";
+const API_URL =
+  process.env.REACT_APP_API_URL || "http://localhost:8000/api/tenders";
 
 // ✅ Fetch tenders with filters
 export const fetchTenders = async (filters = {}) => {
   try {
-    const response = await axios.get(API_URL, { params: filters });
+    const response = await axios.get(`${API_URL}/api/tenders`, {
+      params: filters,
+    });
     return response.data;
   } catch (error) {
     console.error("Error fetching tenders:", error);
